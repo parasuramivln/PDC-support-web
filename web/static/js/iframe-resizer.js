f160b6ae25bbf4410eb094bd01b1ce2daa0741e8
Henric Zazzi <hzazzi@kth.se>
2024-12-11 09:43:50 +0100

    fixed several sections for iframe

--- web/static/js/iframe-resizer.js
+++ web/static/js/iframe-resizer.js
@@ -1,38 +1,41 @@
 let loaded = false;
 
 // Should make iframe goto underlying pages http://<Main URL>/support/?section=/<Folder>/support-docs/basics/quickstart
-// as in https://pdc-web.eecs.kth.se/docs/support/?section=basics/quickstart
+// as in https://pdc-web.eecs.kth.se/docs/support/?section=/doc/support-docs/basics/quickstart
+// or as https://pdc-web.eecs.kth.se/docs/support/?sub=basics/quickstart
 
 function loadPage(iframe) {
     if (!loaded) {
         urlParams = new URLSearchParams(window.location.search);
         section = urlParams.get('section');
         sub =  urlParams.get('sub');
-        if (sub)
-            if (window.location.href.includes('/docs/support'))
+        if (sub) {
+            if (window.location.href.includes('/doc/support'))
                 part_url='/doc/support-docs/';
-            if (window.location.href.includes('/docs/applications'))
+            if (window.location.href.includes('/doc/applications'))
                 part_url='/doc/software-docs/';
-            iframe.src = part_url + sub;        
+            iframe.src = part_url + sub;    
+            }
         if (section)
             iframe.src = section;
         loaded=true;
         resizeIframe(iframe)
+        }
     }
-}
 
 function resizeIframe(iframe) {
     if (iframe) {
         iframe.onload = function() {
             var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
             iframe.style.height = iframeDocument.body.scrollHeight + 'px';
-        };
-    } else {
+            };
+        }
+    else {
         console.error('Iframe with ID ' + frameId + ' not found.');
+        }
     }
-}
 
 function toggleMenu() {
     const nav = document.querySelector('nav ul');
     nav.classList.toggle('open');
-}
+    }
